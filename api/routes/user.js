c3dd2db3dd9f38c03759a42c054ffe2f0a2f316b
vinhabubu <nguyenvinh20112000@gmail.com>
2022-02-07 20:30:10 +0700

    add models

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,13 +1,13 @@
 const router = require("express").Router();
 
-router.get('/usertest', (req, res) =>{
-    res.send("usertest sucessful")
-});
+// router.get('/usertest', (req, res) =>{
+//     res.send("usertest sucessful")
+// });
 
-router.post('/userposttest', (req, res) =>{
-    const username = req.body.username;
-    res.send("username is " + username);
-});
+// router.post('/userposttest', (req, res) =>{
+//     const username = req.body.username;
+//     res.send("username is " + username);
+// });
 
 
 
