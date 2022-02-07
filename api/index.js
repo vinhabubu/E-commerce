78ec3cd37f3e43d3a464b954925305252c060f82
vinhabubu <nguyenvinh20112000@gmail.com>
2022-02-07 20:16:48 +0700

    api

---
+++ api/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+
+const mongoose = require('mongoose');
+
+const dotenv = require('dotenv');
+const userRouter = require('./routes/user')
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL
+    ).then(() => console.log("DBConnect sucessfull")
+    ).catch((err) => {
+        console.log(err);
+    });
+
+app.use(express.json());
+app.use('/api/users', userRouter);
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log("Backend running");
+})
---
+++ api/routes/user.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+
+router.get('/usertest', (req, res) =>{
+    res.send("usertest sucessful")
+});
+
+router.post('/userposttest', (req, res) =>{
+    const username = req.body.username;
+    res.send("username is " + username);
+});
+
+
+
+module.exports = router;
